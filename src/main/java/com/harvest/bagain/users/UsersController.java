f96949c122d362d706664ded35237e26739d9bf1
manyongkim <rlaalsgud04@naver.com>
2024-11-09 00:33:40 +0900

    Add files via upload
    user. 회원가입 추가

--- src/main/java/com/harvest/bagain/users/UsersController.java
+++ src/main/java/com/harvest/bagain/users/UsersController.java
@@ -1,11 +1,36 @@
 package com.harvest.bagain.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@RequestMapping("/users")
 public class UsersController {
-	
-	@Autowired
-	private UsersDAO uDAO;
+
+    @Autowired
+    private UsersDAO usersDAO;
+
+    // 이메일 중복 확인
+    @GetMapping("/check-email")
+    public ResponseEntity<String> checkEmailDuplicate(@RequestParam String email) {
+        String result = usersDAO.checkEmailDuplicate(email);
+        return ResponseEntity.ok(result);
+    }
+
+    // 닉네임 중복 확인
+    @GetMapping("/check-nickname")
+    public ResponseEntity<String> checkNicknameDuplicate(@RequestParam String nickname) {
+        String result = usersDAO.checkNicknameDuplicate(nickname);
+        return ResponseEntity.ok(result);
+    }
+
+    // 사용자 등록
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@Validated @ModelAttribute UserRegisterRequest urr) {
+        String result = usersDAO.registerUser(urr);
+        return ResponseEntity.ok(result);
+    }
 }
--- src/main/java/com/harvest/bagain/users/UsersRepository.java
+++ src/main/java/com/harvest/bagain/users/UsersRepository.java
@@ -1,10 +1,10 @@
 package com.harvest.bagain.users;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsersRepository extends JpaRepository<Users, Integer> {
-	
-	
+public interface UsersRepository extends CrudRepository<Users, Integer> {
+    boolean existsByEmail(String email);
+    boolean existsByNickname(String nickname);
 }
