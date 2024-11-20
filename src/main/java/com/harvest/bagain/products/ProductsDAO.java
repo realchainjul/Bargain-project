e2c2093c849a3d509dd4bf2313c3212e6d1370e9
rlaal <rlawkd23@gmail.com>
2024-11-21 03:01:15 +0900

    product update

--- src/main/java/com/harvest/bagain/products/ProductsDAO.java
+++ src/main/java/com/harvest/bagain/products/ProductsDAO.java
@@ -196,17 +196,29 @@ public class ProductsDAO {
 
 	// 내가 등록한 상품 목록 조회
 	public Map<String, Object> getProductsBySeller(Users seller) {
-		Map<String, Object> response = new HashMap<>();
-		try {
-			List<Products> productsList = prodRepo.findBySeller(seller);
-			response.put("status", true);
-			response.put("products", productsList);
-		} catch (Exception e) {
-			response.put("status", false);
-			response.put("message", "상품 목록 조회 실패");
-		}
-		return response;
-	}
+        Map<String, Object> response = new HashMap<>();
+        try {
+            List<Products> productsList = prodRepo.findBySeller(seller);
+            for (Products product : productsList) {
+                String productImageUrl = product.getPhoto() != null
+                        ? "https://file.bargainus.kr/products/images/" + product.getPhoto()
+                        : "";
+                product.setPhoto(productImageUrl);
+                List<ProductPhoto> productPhotos = productPhotoRepo.findByProduct(product);
+                for (ProductPhoto productPhoto : productPhotos) {
+                    productPhoto.setPhotoUrl(
+                            "https://file.bargainus.kr/productcomment/images/" + productPhoto.getPhotoUrl());
+                }
+                product.setProductPhotos(productPhotos);
+            }
+            response.put("status", true);
+            response.put("products", productsList);
+        } catch (Exception e) {
+            response.put("status", false);
+            response.put("message", "상품 목록 조회 실패");
+        }
+        return response;
+    }
 
 	// 사용자 코드로 사용자 조회
 	public Optional<Users> getUserByCode(Integer userCode) {
